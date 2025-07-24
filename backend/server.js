929ce97a186adc0038f5e3282dc4a0c41759a37e
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-07-24 19:48:55 +0900

    feat : 도메인 적용

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,8 @@ const corsOptions = {
     'https://localhost:3001',
     'https://localhost:3002',
     'http://0.0.0.0:3000',
-    'https://0.0.0.0:3000'
+    'https://0.0.0.0:3000',
+    'https://goorm-ktb-001.goorm.team/'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],

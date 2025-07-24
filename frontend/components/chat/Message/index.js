2713851112785bb94c94d69a3738ee80d2afec84
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-07-24 14:02:33 +0900

    fix : msa 분리 파일 삭제

--- frontend/components/chat/Message/index.js
+++
@@ -1,4 +0,0 @@
-export { default as SystemMessage } from './SystemMessage';
-export { default as FileMessage } from './FileMessage';
-export { default as UserMessage } from './UserMessage';
-export { default as AIMessage } from './AIMessage';
